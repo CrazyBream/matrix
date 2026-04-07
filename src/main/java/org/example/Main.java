4d9e873f4251decc7f440347f043d813d1b50c79
Евгеній Дроздовський <geniya3g@gmail.com>
2026-04-07 23:38:25 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Розробник: Дроздовський Є.І.");
-        System.out.println("------------------------------------");
 
         int rows = 2;
         int cols = 3;
